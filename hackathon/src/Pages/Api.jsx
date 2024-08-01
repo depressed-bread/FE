1e37e3d7915e25b01f7e610b6b64ee5250d027a9
gyozaa01 <olivia110321@gmail.com>
2024-08-01 23:40:06 +0900

    feat: 수정을 위해 로컬로 돌림

--- hackathon/src/Pages/Api.jsx
+++ hackathon/src/Pages/Api.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const host = window.location.hostname === "localhost" 
-  ? 'http://52.79.177.156' // 로컬 환경에서는 직접 서버 URL로 요청
-  : "/api"; // 배포 환경에서는 /api를 사용
+// 환경에 따라 baseURL 설정
+const baseURL = 'http://localhost:8080';
 
 const api = axios.create({
-  baseURL: host,
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
