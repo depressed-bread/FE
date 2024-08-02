24cbd36461544be6cfcdd2b1e6f2a8038794c8ad
gyozaa01 <olivia110321@gmail.com>
2024-08-02 15:54:12 +0900

    feat: 수정하기(이모티콘) 배포

--- hackathon/src/Pages/Api.jsx
+++ hackathon/src/Pages/Api.jsx
@@ -1,27 +1,11 @@
-// import axios from 'axios';
-
-// const host = window.location.hostname === "localhost" 
-//   ? 'http://52.79.177.156' // 로컬 환경에서는 직접 서버 URL로 요청
-//   : "/api"; // 배포 환경에서는 /api를 사용
-
-// const api = axios.create({
-//   baseURL: host,
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
-// });
-
-// export default api;
-
-
 import axios from 'axios';
 
-// 환경에 따라 baseURL 설정
-const baseURL = 'http://localhost:8080';
+const host = window.location.hostname === "localhost" 
+  ? 'http://52.79.177.156' // 로컬 환경에서는 직접 서버 URL로 요청
+  : "/api"; // 배포 환경에서는 /api를 사용
 
 const api = axios.create({
-  baseURL,
+  baseURL: host,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -29,3 +13,19 @@ const api = axios.create({
 });
 
 export default api;
+
+
+// import axios from 'axios';
+
+// // 환경에 따라 baseURL 설정
+// const baseURL = 'http://localhost:8080';
+
+// const api = axios.create({
+//   baseURL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   withCredentials: true, // 세션 쿠키를 포함하기 위해 설정
+// });
+
+// export default api;
