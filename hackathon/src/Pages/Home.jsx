0b34b17e185d780f33db47379d1836cee8248a5d
0GYURI <annie0339@naver.com>
2024-08-03 19:57:47 +0900

    이모지 크기 조절 코드 추가

--- hackathon/src/Pages/Home.jsx
+++ hackathon/src/Pages/Home.jsx
@@ -128,7 +128,9 @@ const EmojiFront = styled.div`
   width: 100%;
   height: 100%;
   background-image: url(${props => props.src});
-  background-size: cover;
+  background-size: contain;
+  background-position:center;
+  background-repeat:no-repeat;
 `;
 const EmojiBack = styled.div`
   backface-visibility: hidden;
